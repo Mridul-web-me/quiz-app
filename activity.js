14275a1bd971fab599669e0102a693c391dfae2d
Mridul Islam <md.mridul.23@gmail.com>
2022-11-10 11:10:11 +0600

    first commit

---
+++ activity.js
@@ -0,0 +1,202 @@
+let jsq_questions = [
+	{
+		"id": 1,
+		"question": "1. There ____ too many children in this class.",
+		"options": {
+			"a": "were",
+			"b": "is",
+			"c": "was",
+			"d": "are"
+		},
+		"answer": "d"
+	},
+	{
+		"id": 2,
+		"question": "2. Forest Gump ____ faster than most people.",
+		"options": {
+			"a": "walk",
+			"b": "talks",
+		},
+		"answer": "b"
+	},
+	{
+		"id": 3,
+		"question": "3. Everybody ____ now present.",
+		"options": {
+			"a": "were",
+			"b": "is",
+			"c": "are",
+		},
+		"answer": "b"
+	},
+	{
+		"id": 4,
+		"question": "4. Anyone ____ able to tell you the right answer.",
+		"options": {
+			"a": "is",
+			"b": "are",
+			"c": "were",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 5,
+		"question": "5. The two girls ____ to look for the missing cat.",
+		"options": {
+			"a": "were going",
+			"b": "is going",
+			"c": "can not",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 6,
+		"question": "6. There ____ some truth in what you say.",
+		"options": {
+			"a": "is",
+			"b": "are",
+			"c": "were",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 7,
+		"question": "7. There ____ anybody in the house. ",
+		"options": {
+			"a": "was not",
+			"b": "were not",
+			"c": "are not",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 8,
+		"question": "8. Where ____ all the money that you gave her?",
+		"options": {
+			"a": "is",
+			"b": "are",
+			"c": "were",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 9,
+		"question": "9. Somebody said that no news ____ good news.",
+		"options": {
+			"a": "is",
+			"b": "are",
+			"c": "were",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 10,
+		"question": "10. They trashed the hotel room and all of the furniture ____ thrown out of the window.",
+		"options": {
+			"a": "was",
+			"b": "have",
+			"c": "were",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 11,
+		"question": "11. There ____ 9 million bicycles in Beijing.",
+		"options": {
+			"a": "are",
+			"b": "was",
+			"c": "is",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 12,
+		"question": "12. All the information he can offer ____ of no use.",
+		"options": {
+			"a": "is",
+			"b": "were",
+			"c": "are",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 13,
+		"question": "13. If he ____ to do that, he would be sorry.",
+		"options": {
+			"a": "were",
+			"b": "was",
+			"c": "is",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 14,
+		"question": "14. He ____ to protect her.",
+		"options": {
+			"a": "is trying",
+			"b": "were trying",
+			"c": "are trying",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 15,
+		"question": "15. She must ____ crazy to people that do not know her.",
+		"options": {
+			"a": "seem",
+			"b": "seems",
+			"c": "look",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 16,
+		"question": "16. My neighbour's dogs ____ all night sometimes.",
+		"options": {
+			"a": "bark",
+			"b": "cries",
+			"c": "shouts",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 17,
+		"question": "17. I ____ sometimes in my spare time.",
+		"options": {
+			"a": "sing",
+			"b": "sings",
+			"c": "runs",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 18,
+		"question": "18. Romeo and Juliet ____ at the end of the play.",
+		"options": {
+			"a": "die",
+			"b": "dies",
+			"c": "commits suicide",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 19,
+		"question": "19. ____ everybody in?",
+		"options": {
+			"a": "Is",
+			"b": "Are",
+			"c": "Do",
+		},
+		"answer": "a"
+	},
+	{
+		"id": 20,
+		"question": "20. The people of Denmark ____ lucky in terms of COVID-19.",
+		"options": {
+			"a": "has been",
+			"b": "have been",
+			"c": "feel",
+		},
+		"answer": "a"
+	},
+];
